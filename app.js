68f26bec335e643adca845ebbc16672d7d945f13
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-20 03:52:50 +0200

    Hours

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2020, 4, 24, 11, 30, 0);
+let futureDate = new Date();
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -36,7 +36,7 @@ let month = futureDate.getMonth();
 month = months[month];
 const date = futureDate.getDate();
 
-const weekday = futureDate.getDate();
+const weekday = weekday[futureDate.getDate()];
 
 
 giveaway.textContent = `giveaway ends on ${date} ${month} ${year} ${hours}:${minutes}am`;
